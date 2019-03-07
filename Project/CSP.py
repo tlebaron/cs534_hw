78f161dca90f31652e36a7170d8552bccdad02a2
tlebaron <thomas.lebaron@free.fr>
2019-03-07 16:31:31 -0500

    almost finished, need to correct the value selection and link the AC3 processing

--- Project/CSP.py
+++ Project/CSP.py
@@ -120,7 +120,7 @@ class Graph:
 	binaryNotEqual = []
 	binaryNotSimultaneous = []
 
-	solution = []
+	possibleAssignments = []
 
 	constraints = [uniform, unaryInclusive, unaryExclusive, binaryEqual, binaryNotEqual, binaryNotSimultaneous]
 
@@ -150,7 +150,10 @@ class Graph:
 			self.variables.append(Variable(var_args))
 		for val in list_values:
 			self.values.append(Value(val))
-		self.solution = [None]*len(list_variables)
+		for var in self.variables:
+			self.possibleAssignments.append([var, []])
+			for val in self.values:
+				self.possibleAssignments[-1][1].append(val)
 
 
 		self.uniform.append(Constraint(self, 0, self.variables, constraints_deadline))
@@ -167,6 +170,7 @@ class Graph:
 				if unary_constraint.variables[0] in binary_constraint.variables:
 					binary_constraint.updateBinaryConstraint(self, unary_constraint)
 
+		self.initializePossibleAssignments()
 
 
 	def addUnaryConstraint(self,type_of_constraint, list_constraints):
@@ -216,48 +220,109 @@ class Graph:
 			if type_of_constraint == 2:
 				self.binaryNotSimultaneous.append(Constraint(self, 23, constraintVar, constraintValues))
 
-	def selectVariable(self):
-		# return which variable to expand
-		# return the variable with the minimum remaining value
+	def initializePossibleAssignments(self):
+		# initialize the possible assignment with the initial constraints
+		# constraints = [list_constraints_1, list_constraints_2, ...]
+		for [var, assignments] in self.possibleAssignments:
+			for constraint_list in self.constraints[1:]: #TODO: increment if unary constraints start not at constraints[1]
+				for constraint in constraint_list:
+					if var not in constraint.variables:
+						continue
+					elif constraint.type > 9 and constraint.type < 20:
+						for i, isPossible in enumerate(constraint.values):
+							if not isPossible and self.values[i] in assignments:
+								assignments.remove(self.values[i])
+					elif constraint.type > 19:
+						if var == constraint.variables[0]:
+							for i in range(len(constraint.values)):
+								isPossible = False
+								for j in range(len(constraint.values[0])):
+									if constraint.values[i][j] == 1:
+										isPossible = True
+										break
+								if not isPossible and self.values[i] in assignments:
+									assignments.remove(self.values[i])
+						elif var == constraint.variables[1]:
+							for i in range(len(constraint.values[0])):
+								isPossible = False
+								for j in range(len(constraint.values)):
+									if constraint.values[j][i] == 1:
+										isPossible = True
+										break
+								if not isPossible and self.values[i] in assignments:
+									assignments.remove(self.values[i])
+						else:
+							assert False
+
+
+	def selectVariable(self, assignment):
+		# assignment contains variable already assigned
+		# return the variable to expand in order of expansion
+		# return the variable with the minimum remaining value first
 		# if tie, select the one with the most constraints
 
-		minRemainingValues = self.minRemainingValues(self.selectRemainingValues())
+		minRemainingValues = self.minRemainingValues(self.selectRemainingValues(), assignment)
 
-		if len(minRemainingValues) == 1:
-			return minRemainingValues[0]
+		self.maxConstraintedValues(minRemainingValues, assignment)
+		# remainingValue = [[var, #possible values, #constraints], ...]
+
+		#print sorted(sorted(minRemainingValues, key = lambda x: x[2]), key = lambda y: y[1])
+
+		return sorted(sorted(minRemainingValues, key = lambda x: x[2], reverse = True), key = lambda y: y[1])
 
 		# minRemainingValues is a set of [var, possibleValues]
 		# decide between them with the degree heuristic: the most constraint var goes first.
 		# simply sort by number of time the variable is present in a binary constraint
-
+		"""
 		numberOfConstraints = [0]*len(minRemainingValues)
 		for i, [var, minRemVal] in enumerate(minRemainingValues):
 			for constraintType in self.constraints[3:]: #TODO: change if there is more than 3 non binary constraints!
 				for constraint in constraintType:
 					if var in constraint.variables:
 						numberOfConstraints[i] += 1
-
+		
 		return minRemainingValues[numberOfConstraints.index(max(numberOfConstraints))][0]
+		"""
 
-	def minRemainingValues(self, remainingValues):
+	def maxConstraintedValues(self, setVariables, assignment):
+		for i in range(len(setVariables)):
+			setVariables[i].append(-1)
+		for i, [var, minRemVal, init] in enumerate(setVariables):
+			numberOfConstraints = 0
+			for constraintType in self.constraints[3:]: #TODO: change if there is more than 3 non binary constraints!
+				for constraint in constraintType:
+					if var in constraint.variables:
+						numberOfConstraints += 1
+			setVariables[i][2] = numberOfConstraints
+		return setVariables
+
+
+	def minRemainingValues(self, remainingValues, assignment):
 		numberRemainingValues = [sum(x) for x in remainingValues]
-		minRemainingValues = [[False, len(self.values)+1]]
+		minRemainingValues = []
+
 		for i in range(len(numberRemainingValues)):
-			if self.solution[i] != None: # if we already found a solution for the variable i, it must not be added
-											# TODO: too much dependencies, should use dict instead of list
-											# this in solution verification should be in selectRemainingValues()
-				continue
+			isAssigned = False
+			for assignedVariable in assignment:
+				if assignedVariable[0] == self.variables[i]:
+					isAssigned = True
+					break
+
+			if not isAssigned:
+				minRemainingValues.append([self.variables[i], numberRemainingValues[i]])
+			"""
 			if numberRemainingValues[i] < minRemainingValues[0][1]:
 				minRemainingValues = [[self.variables[i], numberRemainingValues[i]]]
 			elif numberRemainingValues[i] == minRemainingValues[0][1]:
 				minRemainingValues.append([self.variables[i], numberRemainingValues[i]])
+			"""
 
 		return minRemainingValues
 
 	def selectRemainingValues(self, additionalConstraints = []):
 
 		# return the remaining values for each variable with the up to date constraints
-		# can send additional constraints, which will not be stored in the self.constraints variable
+		# the additional constraint act as a single unary inc constraint to simulate the assignment of the variable
 
 		remainingValues = [[1 for x in range(len(self.values))] for y in range(len(self.variables))]
 
@@ -289,22 +354,141 @@ class Graph:
 						remainingValues[varIndex1][i] = remainingValues[varIndex1][i] and remaining1
 						remainingValues[varIndex2][i] = remainingValues[varIndex2][i] and remaining2
 
+		#print "remainingValues {0}".format(remainingValues)
+
 		return remainingValues
 
-	def selectValue(self, variable):
+	def selectValue(self, variable, assignment):
 		# least constraining value heuristic:
+		# sort the values in order of increment constraints
 		# for all the value possible for this variable, count the number of possible values for the other variables
 		# and pick the one with the highest minimum
 
-		remainingValues = self.selectRemainingValues()
-		minPossibleValues = [-1]*len(self.values)
-		for i, val in enumerate(self.values):
-			if remainingValues[self.variables.index(variable)] == 0:
-				continue
-			minPossibleValues[i] = self.minRemainingValues(
-									self.selectRemainingValues([Constraint(self, 11, [variable], [val])]))[0][1]
 
-		return self.values[minPossibleValues.index(max(minPossibleValues))]
+		numberRemainingValues = [-1]*len(self.variables)
+		minNumberRemainingValues = []
+		for i, val in enumerate(self.possibleAssignments[self.variables.index(variable)][1]):
+			minNumberRemainingValues.append([val])
+			minNumberRemainingValues[-1].append(self.minRemainingValues(
+												self.selectRemainingValues([Constraint(self, 11, [variable], [val])]), 
+												assignment
+												)[0][1]
+												)
+
+		return sorted(minNumberRemainingValues,key = lambda x: x[1])
+
+	def consistentAssignment(self, variable, value, assignment):
+
+		varIndex = self.variables.index(variable)
+		valIndex = self.values.index(value)
+
+
+		for list_const in self.constraints :
+			if len(list_const) == 0:
+				continue
+
+			for const in list_const:
+				if variable not in const.variables:
+					continue
+				if list_const[0].type == 0:
+					length = 0
+					for assign in assignment:
+						if assign[1] == value:
+							length += int(assign[0].args) 						#TODO change if there is no deadline constraint!!
+					return int(const.values[0]) > length + int(variable.args)
+				if const.type < 20:
+					return const.values[valIndex] == 1
+				else:
+					if variable == const.variables[0]:
+						for j in range(len(const.values[0])):
+							if const.values[valIndex][j] == 1:
+								return True
+						return False
+
+					if variable == const.variables[0]:
+						for j in range(len(const.values)):
+							if const.values[j][valIndex] == 1:
+								return True
+						return False
+
+		return True
+
+def removeInconsistentValue(graph, arc):
+	# arc = [value assigned, value linked, binary constraint, values in order in constraint]
+	removed = False
+	for possibleAssignment in graph.possibleAssignments:
+		if arc[0] == possibleAssignment[0]:
+			domain1 = possibleAssignment[1]
+			break
+	for possibleAssignment in graph.possibleAssignments:
+		if arc[1] == possibleAssignment[0]:
+			domain2 = possibleAssignment[1]
+			break
+
+	for possibleValue1 in domain1:
+		satisfied = False
+		for possibleValue2 in domain2:
+			#if Value1 and Value2 satisfy the constraint then  turn satisfied to True
+			if arc[3] == 0:
+				if constraint[graph.values.index(possibleValue1)][graph.values.index(possibleValue1)] == 1:
+					satisfied = True
+					break
+			if arc[3] == 1:
+				if constraint[graph.values.index(possibleValue2)][graph.values.index(possibleValue1)] == 1:
+					satisfied = True
+					break
+		if not satisfied:
+			domain1.remove(possibleValue1)
+			removed = True
+	return removed
+
+
+def constraintPropagation(graph, variableAssigned):
+	# create the queue of arcs, corresponding to the binary constraints
+	queue = []
+	for constraint_list in graph.constraints:
+		if constraint_list[0].type > 19:
+			for constraint in constraint_list:
+				if variableAssigned == constraint.variables[0]:
+					queue.append([variableAssigned, constraint.variables[1], constraint, 0])
+				elif variableAssigned == constraint.variables[1]:
+					queue.append([variableAssigned, constraint.variables[0], constraint, 1])
+
+	while len(queue) > 0:
+		arc = queue[0]
+		del queue[0]
+		if removedInconsistentValue(graph, arc):
+			for constraint_list in graph.constraints:
+				if constraint_list[0].type > 19:
+					for constraint in constraint_list:
+						if arc[1] == constraint.variables[0]:
+							queue.append([arc[1], constraint.variables[1], constraint, 0])
+						elif arc[1] == constraint.variables[1]:
+							queue.append([arc[1], constraint.variables[0], constraint, 1])
+
+
+def backtrackingSearch(graph):
+	return recursiveBacktracking([], graph)
+
+def recursiveBacktracking(assignment, graph):
+	if len(assignment) == len(graph.variables):
+		return assignment
+
+	for [var, numRemainVal, numConstr] in graph.selectVariable(assignment):
+		print "variable selected: {0}".format(var)
+		for [val, minNumb] in graph.selectValue(var, assignment):
+			print "test with value {0}".format(val)
+			if graph.consistentAssignment(var, val, assignment):
+				print "{0} assigned {1} is consistent, test the children".format(var, val)
+				assignment.append([var, val])
+				result = recursiveBacktracking(assignment, graph)
+				if result != False:
+					return result
+				assignment.remove([var, val])
+				print "no solution with {0} assigned to {1}".format(var, val)
+			print "{0} assigned {1} is not consistent, test next variable".format(var, val)
+		return False
+
 
 
 
@@ -332,11 +516,18 @@ def main():
 		else:
 			input_list[-1].append(line)
 
+	print input_list
+
 	graph = Graph(input_list[0], input_list[1], input_list[2], input_list[3], input_list[4], input_list[5],
 					input_list[6], input_list[7])
 
 
-	print graph.selectValue(graph.selectVariable())
+	solution =  backtrackingSearch(graph)
+	if solution:
+		print solution
+	else:
+		print "no solution"
+
 
 if __name__ == '__main__':
 	main()
